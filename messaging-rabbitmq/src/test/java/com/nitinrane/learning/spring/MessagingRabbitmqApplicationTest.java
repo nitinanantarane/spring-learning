612e7818e20a8fcf8a94fb7710c310604b9625a1
nitin rane <nitinanantarane@gmail.com>
2022-08-24 11:55:30 +0530

    test class

---
+++ messaging-rabbitmq/src/test/java/com/nitinrane/learning/spring/MessagingRabbitmqApplicationTest.java
@@ -0,0 +1,37 @@
+package com.nitinrane.learning.spring;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class MessagingRabbitmqApplicationTest {
+
+	@MockBean
+	private Runner runner;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private Receiver receiver;
+
+	@Test
+	public void test() throws Exception {
+		try {
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.queueName,
+				"Hello from RabbitMQ!");
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
+		catch (AmqpConnectException e) {
+			// ignore - rabbit is not running
+		}
+	}
+
+}
