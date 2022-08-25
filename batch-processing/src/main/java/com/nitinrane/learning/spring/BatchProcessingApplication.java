9c69d90c9d9e3ad7ec9531d73c96c076e965d707
nitin rane <nitinanantarane@gmail.com>
2022-08-25 15:19:29 +0530

    batch processing

---
+++ batch-processing/src/main/java/com/nitinrane/learning/spring/BatchProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.nitinrane.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(
+			SpringApplication.run(BatchProcessingApplication.class, args)));
+	}
+
+}
