945e1c0000df2651d002cd418fc609e5fcbe17df
nitin rane <nitinanantarane@gmail.com>
2022-08-25 16:03:34 +0530

    rest hateoas

---
+++ rest-hateoas/src/main/java/com/nitinrane/learning/spring/RestHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.nitinrane.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHateoasApplication.class, args);
+	}
+
+}
